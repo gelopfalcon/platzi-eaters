285567d193ae006e3e3313d68ff74fd877ff2350
Falcon <>
2022-04-20 09:48:32 -0600

    Add test

--- platzi-eaters/contracts/test/sample-test.js
+++ platzi-eaters/contracts/test/sample-test.js
@@ -1,8 +1,9 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("PlatziFood", function () {
+describe("PlatziFood", async function () {
   it("Add a new dish", async function () {
+    const [owner, addr1] = await ethers.getSigners();
     const PlatziFood = await ethers.getContractFactory("PlatziFood");
     const platziFood = await PlatziFood.deploy();
     await platziFood.deployed();
@@ -16,8 +17,18 @@ describe("PlatziFood", function () {
     //esperar que el addPlatziFood method sea minteado
     await addFood.wait();
 
+    var addFood2 = await platziFood
+      .connect(addr1)
+      .addPlatziFood(
+        "https://eatyourworld.com/images/content_images/images/gallo-pinto.jpg",
+        "Gallo Pinto2",
+        "Costa Rica"
+      );
+
+    await addFood2.wait();
+
     var foods = await platziFood.getAllPlatziFoods();
-    expect(foods.length).to.equal(1);
+    expect(foods.length).to.equal(2);
 
     var foodsByOwner = await platziFood.getPlatziFoodsByOwner();
 
